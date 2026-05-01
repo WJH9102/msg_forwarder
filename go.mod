099f13e1df1b820c54dd8a399fb0be974a3596e2
jhwang31 <jhwang31@iflytek.com>
2026-05-01 14:21:40 +0800

    refactor: replace gomail with net/smtp for serverless compat

--- go.mod
+++ go.mod
@@ -2,9 +2,4 @@ module github.com/WJH9102/msg_forwarder
 
 go 1.26.1
 
-require (
-	github.com/joho/godotenv v1.5.1
-	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
-)
-
-require gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+require github.com/joho/godotenv v1.5.1
--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -3,9 +3,10 @@ package mailer
 import (
 	"crypto/tls"
 	"fmt"
+	"net/smtp"
+	"strings"
 
 	"github.com/WJH9102/msg_forwarder/internal/config"
-	"gopkg.in/gomail.v2"
 )
 
 type Mailer struct {
@@ -17,19 +18,57 @@ func New(cfg *config.Config) *Mailer {
 }
 
 func (m *Mailer) Send(to, subject, body string) error {
-	msg := gomail.NewMessage()
-	msg.SetHeader("From", fmt.Sprintf("%s <%s>", m.cfg.SenderName, m.cfg.SMTPUser))
-	msg.SetHeader("To", to)
-	msg.SetHeader("Subject", subject)
-	msg.SetBody("text/plain", body)
+	from := m.cfg.SMTPUser
+	addr := fmt.Sprintf("%s:%d", m.cfg.SMTPHost, m.cfg.SMTPPort)
 
-	dialer := gomail.NewDialer(
-		m.cfg.SMTPHost,
-		m.cfg.SMTPPort,
-		m.cfg.SMTPUser,
-		m.cfg.SMTPPassword,
-	)
-	dialer.TLSConfig = &tls.Config{ServerName: m.cfg.SMTPHost}
+	var msg strings.Builder
+	msg.WriteString("From: " + from + "\r\n")
+	msg.WriteString("To: " + to + "\r\n")
+	msg.WriteString("Subject: " + subject + "\r\n")
+	msg.WriteString("\r\n")
+	msg.WriteString(body)
 
-	return dialer.DialAndSend(msg)
+	auth := smtp.PlainAuth("", from, m.cfg.SMTPPassword, m.cfg.SMTPHost)
+
+	tlsConfig := &tls.Config{ServerName: m.cfg.SMTPHost}
+
+	conn, err := tls.Dial("tcp", addr, tlsConfig)
+	if err != nil {
+		return fmt.Errorf("tls dial: %w", err)
+	}
+	defer conn.Close()
+
+	client, err := smtp.NewClient(conn, m.cfg.SMTPHost)
+	if err != nil {
+		return fmt.Errorf("smtp new client: %w", err)
+	}
+	defer client.Close()
+
+	if err = client.Auth(auth); err != nil {
+		return fmt.Errorf("smtp auth: %w", err)
+	}
+
+	if err = client.Mail(from); err != nil {
+		return fmt.Errorf("smtp mail: %w", err)
+	}
+
+	if err = client.Rcpt(to); err != nil {
+		return fmt.Errorf("smtp rcpt: %w", err)
+	}
+
+	w, err := client.Data()
+	if err != nil {
+		return fmt.Errorf("smtp data: %w", err)
+	}
+
+	_, err = w.Write([]byte(msg.String()))
+	if err != nil {
+		return fmt.Errorf("smtp write: %w", err)
+	}
+
+	if err = w.Close(); err != nil {
+		return fmt.Errorf("smtp close data: %w", err)
+	}
+
+	return client.Quit()
 }
